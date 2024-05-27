008686a865787645661f35e0b84d0f1787c2e923
SridharVadla <vadlasreedhar123@gmail.com>
2024-05-27 15:24:11 +0530

    Introduced Spring DATA JPA

--- src/main/java/com/java100xdev/JobPortal/job/impl/JobServiceImpl.java
+++ src/main/java/com/java100xdev/JobPortal/job/impl/JobServiceImpl.java
@@ -1,7 +1,9 @@
 package com.java100xdev.JobPortal.job.impl;
 
 import com.java100xdev.JobPortal.job.Job;
+import com.java100xdev.JobPortal.job.JobRepository;
 import com.java100xdev.JobPortal.job.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -11,45 +13,59 @@ import java.util.stream.Stream;
 
 @Service
 public class JobServiceImpl implements JobService {
-    private List<Job> jobs = new ArrayList<>();
+    @Autowired
+    private JobRepository jobRepository;
+
 
     @Override
     public List<Job> getJobs() {
-        return jobs;
+        return jobRepository.findAll();
     }
 
     @Override
     public void createJob(Job job) {
-        job.setId(jobs.size() + 1);
-        jobs.add(job);
+        jobRepository.save(job);
     }
 
     @Override
     public Job getJob(int id) {
+        jobRepository.findById(id);
 
-        Optional<Job> optionalJob = jobs.stream().filter(job -> job.getId() == id).findFirst();
+        Optional<Job> optionalJob = jobRepository.findById(id);
 
         return optionalJob.orElse(null);
     }
 
     @Override
     public boolean deleteJob(int id) {
-        return jobs.removeIf(job -> job.getId() == id);
+        try {
+            jobRepository.deleteById(id);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
     @Override
     public boolean updateJob(Job job, int id) {
-        Optional<Job> optionalJob = jobs.stream().filter(job1 -> job1.getId() == id).findFirst();
-        if (optionalJob.isPresent()) {
-            Job job1 = optionalJob.get();
-            job1.setTitle(job.getTitle());
-            job1.setJobDescription((job.getJobDescription()));
-            job1.setLocation(job.getLocation());
-            job1.setMinSalary(job.getMinSalary());
-            job1.setMaxSalary(job.getMaxSalary());
-            return true;
+        try {
+            Optional<Job> optionalJob = jobRepository.findById(id);
+            if (optionalJob.isPresent()) {
+                Job job1 = optionalJob.get();
+                job1.setTitle(job.getTitle());
+                job1.setJobDescription((job.getJobDescription()));
+                job1.setLocation(job.getLocation());
+                job1.setMinSalary(job.getMinSalary());
+                job1.setMaxSalary(job.getMaxSalary());
+                jobRepository.save(job1);
+                return true;
 
+            }
+        } catch (Exception e) {
+            return false;
         }
+
         return false;
     }
 }
